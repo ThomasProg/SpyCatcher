16cb23fb3277918b369e222e26565208d2e2c08f
Thomas DALLARD <progz.email@gmail.com>
2021-09-07 10:17:55 +0200

    Default Unity Project added, Default layout added, Drag and Drop added

---
+++ Assets/Scripts/MyDragScript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class DragAndDropManager
+{
+    List<MyDragScript> draggables = new List<MyDragScript>();
+
+    public void SetPriorityOrderOnTop(MyDragScript draggable)
+    {
+        draggable.image.transform.SetAsLastSibling();
+    }
+
+    public void RegisterDraggable(MyDragScript draggable)
+    {
+        //draggables.Add(draggable);
+    }
+
+    public void UnregisterDraggable(MyDragScript draggable)
+    {
+        //draggables.Remove(draggable);
+    }
+};
+
+
+public class MyDragScript : MonoBehaviour
+{
+    bool isDragged = false;
+    Vector3 lastPosition; 
+
+    internal UnityEngine.UI.Image image = null;
+
+    static DragAndDropManager dragAndDropManager = new DragAndDropManager();
+
+    public List<RectTransform> dragArea = new List<RectTransform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<UnityEngine.UI.Image>();
+        dragAndDropManager.RegisterDraggable(this);
+    }
+
+    private void OnDestroy()
+    {
+        dragAndDropManager.UnregisterDraggable(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragged /*&& IsInDraggableArea(Input.mousePosition)*/)
+        {
+            transform.position = Input.mousePosition; 
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        lastPosition = image.transform.position;
+        isDragged = true;
+        dragAndDropManager.SetPriorityOrderOnTop(this);
+    }
+
+    bool IsPointInDraggableArea(Vector3 point)
+    {
+        foreach (RectTransform im in dragArea)
+        {
+            if (point.x < im.transform.position.x + im.rect.width / 2
+                && point.y < im.transform.position.y + im.rect.height / 2 
+                && point.x > im.transform.position.x - im.rect.width / 2
+                && point.y > im.transform.position.y - im.rect.height / 2)
+                return true;
+        }
+        return false;
+    }
+
+    bool IsInDraggableArea(Vector3 imagePos)
+    {
+        Vector3 p1 = imagePos + new Vector3(- image.rectTransform.rect.width / 2, - image.rectTransform.rect.height / 2);
+        Vector3 p2 = imagePos + new Vector3(- image.rectTransform.rect.width / 2, image.rectTransform.rect.height / 2);
+        Vector3 p3 = imagePos + new Vector3(image.rectTransform.rect.width / 2, - image.rectTransform.rect.height / 2);
+        Vector3 p4 = imagePos + new Vector3(image.rectTransform.rect.width / 2, image.rectTransform.rect.height / 2);
+        return IsPointInDraggableArea(p1) && IsPointInDraggableArea(p2) && IsPointInDraggableArea(p3) && IsPointInDraggableArea(p4);
+    }
+
+    public void OnMouseUp()
+    {
+        isDragged = false;
+        if (!IsInDraggableArea(image.transform.position))
+        {
+            transform.position = lastPosition;
+        }
+
+    }
+}
