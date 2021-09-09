c16e21e4175c3d025ddcd440bc48f8fad55ca8df
Thomas DALLARD <progz.email@gmail.com>
2021-09-09 15:33:20 +0200

    Racerule added

---
+++ Assets/Scripts/CustomRules/RaceRule.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceRule : Rule
+{
+    [SerializeField]
+    string race;
+
+    public override string CheckIncoherence(InspectionItem item)
+    {
+        switch (item.info.type)
+        {
+            case DataType.Race:
+            {
+                if (race == item.info.value)
+                {
+                    return "Anomalie détectée.";
+                }
+                else
+                    return null;
+            }
+        }
+        return null;
+    }
+}
